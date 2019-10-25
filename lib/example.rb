fef4f7c63af7e70e253cc99b70188beb9a2380c8
Joseph Miller <jdworldtv@gmail.com>
2019-10-25 00:37:58 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,17 +1,6 @@
 def say_hello_world_ten_times
   phrase ="hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
 end
-
 say_hello_world_ten_times
 
 # ... The rest of the program
